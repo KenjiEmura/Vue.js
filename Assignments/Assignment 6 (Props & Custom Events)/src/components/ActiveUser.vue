21ba7223dccb4f30bb0a86d3c9efbd65286b2e02
Kenji Emura <kenjiemura@gmail.com>
2021-01-28 18:57:43 +0900

    Finished Assignment #6

---
+++ Assignments/Assignment 6 (Props & Custom Events)/src/components/ActiveUser.vue
@@ -0,0 +1,24 @@
+<template>
+    <li>
+        <h2>{{ name }}</h2>
+        <h3>Age: {{ age }}</h3>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        id: {
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        age: {
+            required: true,
+        },
+    },
+};
+</script>
+
---
+++ Assignments/Assignment 6 (Props & Custom Events)/src/components/UserData.vue
@@ -0,0 +1,25 @@
+<template>
+    <form @submit.prevent="$emit('addUser', name, age)">
+        <div>
+            <label>Name:</label>
+            <input v-model="name" type="text">
+        </div>
+        <div>
+            <label>Age</label>
+            <input v-model="age" type="number">
+        </div>
+        <button>Add new user</button>
+    </form>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            name: '',
+            age: '',
+        }
+    },
+}
+</script>
