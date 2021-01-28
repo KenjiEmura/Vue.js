f66c48814e40155df508b36d1ffc1d54397ae8b8
Kenji Emura <kenjiemura@gmail.com>
2021-01-28 11:54:54 +0900

    Passing data Parent -> Child

---
+++ new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,56 @@
+<template>
+    <li>
+        <h2>{{ name }} {{ friendIsFavorite ? "(Favorite)" : "" }}</h2>
+        <button @click="toggleFavorite">Toggle Favorite</button>
+        <button @click="toggleDetails">
+            {{ detailsAreVisible ? "Hide" : "Show Details" }}
+        </button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+            <li><strong>Email:</strong> {{ emailAddress }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    // props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+    props: {
+        name: {
+            type: String,
+            required: true,
+            default: 'NO NAME PASSED'
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        emailAddress: {
+            type: String,
+            required: true,
+        },
+        isFavorite: {
+            type: Boolean,
+            required: false,
+            default: false, // A function can be used here...
+            // validator: value => {
+            //     return value === true || value === false
+            // }
+        },
+    },
+    data() {
+        return {
+            detailsAreVisible: false,
+            friendIsFavorite: this.isFavorite,
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        },
+        toggleFavorite() {
+            this.friendIsFavorite = !this.friendIsFavorite;
+        },
+    },
+};
+</script>
