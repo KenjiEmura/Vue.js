e3d4c44e1e481db52277208080b5a33d8cdd4d39
Kenji Emura <kenjiemura@gmail.com>
2021-01-30 21:24:00 +0900

    Created the "Advanced Components" project

---
+++ Walkthroughs/Components (Advanced)/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <TheHeader />
+        <BadgeList/>
+        <user-info
+            v-for="user in activeUser"
+            :key="user.id"
+            v-bind="user"
+        ></user-info>
+    </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
+import BadgeList from './components/BadgeList.vue';
+
+export default {
+    components: {
+        TheHeader,
+        UserInfo,
+        BadgeList,
+    },
+    data() {
+        return {
+            activeUser: [
+                {
+                    id: "maximilian",
+                    name: "Maximilian Schwarzmüller",
+                    description: "Site owner and admin",
+                    role: "admin",
+                },
+                {
+                    id: "kenji",
+                    name: "Kenji Emura",
+                    description: "Un colado que intenta aprender a programar",
+                    role: "author",
+                },
+            ],
+        };
+    },
+};
+</script>
+
+<style>
+html {
+    font-family: sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
---
+++ Walkthroughs/Components (Advanced)/src/components/UserInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <base-card>
+      <template v-slot:header>
+        <h3>{{ name }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <template v-slot:default>
+        <p>{{ description }}</p>
+      </template>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import BaseCard from "./BaseCard.vue";
+export default {
+  components: { BaseCard },
+  props: ["name", "description", "role"],
+};
+</script>
+ 
+<style scoped>
+section div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
