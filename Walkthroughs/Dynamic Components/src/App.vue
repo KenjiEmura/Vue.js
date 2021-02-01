7f0cc1da4d49149389584a7e0ac3d23997da1ff1
Kenji Emura <kenjiemura@gmail.com>
2021-02-01 12:52:50 +0900

    Teleporting and Fragments

--- Walkthroughs/Dynamic Components/src/App.vue
+++ Walkthroughs/Dynamic Components/src/App.vue
@@ -1,70 +1,63 @@
 <template>
-    <div>
-        <TheHeader />
-        <BadgeList/>
-        <user-info
-            v-for="user in activeUser"
-            :key="user.id"
-            v-bind="user"
-        ></user-info>
-        <!-- <course-goals>
-          <template #default="slotProps">
-            <h2 @click="console(slotProps)">{{ slotProps.item }}</h2>
-            <p>{{ slotProps.anotherProp }}</p>
-          </template>
-        </course-goals> -->
-        <course-goals #default="slotProps">
-            <h2 @click="console(slotProps)">{{ slotProps.item }}</h2>
-            <p>{{ slotProps.anotherProp }}</p>
-        </course-goals>
-    </div>
+  <div>
+    <TheHeader />
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <ActiveGoals v-if="selectedComponent === 'active'"/>
+    <ManageGoals v-if="selectedComponent === 'manage'"/> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+  </div>
 </template>
 
 <script>
-import TheHeader from "./components/TheHeader.vue";
-import UserInfo from "./components/UserInfo.vue";
-import BadgeList from './components/BadgeList.vue';
-import CourseGoals from './components/CourseGoals.vue';
+import TheHeader from "./components/layout/TheHeader.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
-    components: {
-        TheHeader,
-        UserInfo,
-        BadgeList,
-        CourseGoals,
+  components: {
+    TheHeader,
+    ActiveGoals,
+    ManageGoals,
+  },
+  data() {
+    return {
+      selectedComponent: "active-goals",
+      activeUser: [
+        {
+          id: "maximilian",
+          name: "Maximilian Schwarzmüller",
+          description: "Site owner and admin",
+          role: "admin",
+        },
+        // {
+        //     id: "kenji",
+        //     name: "Kenji Emura",
+        //     description: "Un colado que intenta aprender a programar",
+        //     role: "author",
+        // },
+      ],
+    };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
     },
-    data() {
-        return {
-            activeUser: [
-                {
-                    id: "maximilian",
-                    name: "Maximilian Schwarzmüller",
-                    description: "Site owner and admin",
-                    role: "admin",
-                },
-                // {
-                //     id: "kenji",
-                //     name: "Kenji Emura",
-                //     description: "Un colado que intenta aprender a programar",
-                //     role: "author",
-                // },
-            ],
-        };
-    },
-    methods: {
-      console(value) {
-        console.log(value)
-      }
-    }
+  },
 };
 </script>
 
 <style>
 html {
-    font-family: sans-serif;
+  font-family: sans-serif;
 }
 
 body {
-    margin: 0;
+  margin: 0;
 }
 </style>
