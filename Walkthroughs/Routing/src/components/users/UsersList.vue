bd9628fb751d1f77fb52e781cdb1412aff2f2def
Kenji Emura <kenjiemura@gmail.com>
2021-02-04 16:03:45 +0900

    Fixed Routing Bug

---
+++ Walkthroughs/Routing/src/components/users/UsersList.vue
@@ -0,0 +1,41 @@
+<template>
+  <button @click="confirmInput">Confirm</button>
+  <ul>
+    <user-item
+      v-for="user in users"
+      :key="user.id"
+      v-bind="user"
+    ></user-item>
+  </ul>
+</template>
+
+<script>
+import UserItem from "./UserItem.vue";
+
+export default {
+  components: {
+    UserItem,
+  },
+  inject: ["users"],
+  methods: {
+    confirmInput() {
+      // do something
+
+      // this.$router es para los links, .$route es para acceder a la query string
+      this.$router.push("/teams");
+      // this.$router.back();
+      // this.$router.forward();
+      // ....
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  max-width: 20rem;
+  padding: 0;
+}
+</style>
