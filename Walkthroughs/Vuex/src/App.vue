683eddef12d6ce894365dff332f89c927b9665e0
Kenji Emura <kenjiemura@gmail.com>
2021-02-08 20:23:39 +0900

    Vuex, mutations and payloads

---
+++ Walkthroughs/Vuex/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <base-container title="Vuex">
+    <!-- <h3>{{ $store.state.counter }}</h3> -->
+    <the-counter></the-counter>
+    <button @click="addOne">Add 10</button>
+    <change-counter></change-counter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    }
+  },
+  methods: {
+    addOne() {
+      // this.$store.commit('increase', { value: 10 });
+      this.$store.commit({
+        type: 'increase',
+        value: 10,
+      })
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
