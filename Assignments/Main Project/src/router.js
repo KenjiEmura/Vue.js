f11f18c29342f72c885f0df4edc82c7f3fc2abde
Kenji Emura <kenjiemura@gmail.com>
2021-02-26 22:42:00 +0900

    Added Async Components

--- Assignments/Main Project/src/router.js
+++ Assignments/Main Project/src/router.js
@@ -1,13 +1,20 @@
+// import { defineAsyncComponent } from 'vue'; // DON'T USE ASYNC COMPONENTS WITH ROUTING
 import { createRouter, createWebHistory } from 'vue-router';
 import store from './store/index.js';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
-import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+// import CoachDetail from './pages/coaches/CoachDetail.vue';
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import ContactCoach from './pages/requests/ContactCoach.vue';
+// import RequestsReceived from './pages/requests/RequestsReceived.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
+
+const CoachDetail =       () => import('./pages/coaches/CoachDetail.vue')
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue')
+const ContactCoach =      () => import('./pages/requests/ContactCoach.vue')
+const RequestsReceived =  () => import('./pages/requests/RequestsReceived.vue')
+const UserAuth =          () => import('./pages/auth/UserAuth.vue')
 
 const router = createRouter({
   history: createWebHistory(),
